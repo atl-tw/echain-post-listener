f9ffaff0216df25a34b72049d94209a23be6d510
Kiana Tennyson <ktenny@thoughtworks.com>
2016-01-27 11:37:34 -0500

    switch to php

---
+++ postListener.php
@@ -0,0 +1,19 @@
+<?php
+
+use Mosquitto\Client;
+
+/*when this php file is hit, it attempts to connect, puts message on queue, then disconnects*/
+
+$client = new Mosquitto\Client('PostListenerClient');
+$topic = 'office/uhfrfid/echain';
+$qos = 2;
+$retain = false;
+
+/* when connection is complete, send the received $_POST */
+$client->onConnect(function() use ($client) {
+    $client->publish($topic, 'publish from php', $qos, $retain);
+});
+
+$client->connect('atliot.com', 1883);
+
+?>
