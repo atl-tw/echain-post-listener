63d3a3696e90f6e5e0226a9e9b834bf6a78659d1
Kiana Tennyson <ktenny@thoughtworks.com>
2016-01-27 12:27:40 -0500

    fixing variable scope

--- postListener.php
+++ postListener.php
@@ -5,12 +5,12 @@ use Mosquitto\Client;
 /*when this php file is hit, it attempts to connect, puts message on queue, then disconnects*/
 
 $client = new Mosquitto\Client('PostListenerClient');
-$topic = 'office/uhfrfid/echain';
-$qos = 2;
-$retain = false;
 
 /* when connection is complete, send the received $_POST */
 $client->onConnect(function() use ($client) {
+    $topic = 'office/uhfrfid/echain';
+    $qos = 2;
+    $retain = false;
     $client->publish($topic, 'publish from php', $qos, $retain);
 });
 
